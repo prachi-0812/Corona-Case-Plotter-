a7144d1f016d19ca92ac6b9b21b9b667dc258a72
prachi-0812 <108670915+prachi-0812@users.noreply.github.com>
2022-07-04 17:34:27 +0530

    Add files via upload

---
+++ Corona cases plotter/index.js
@@ -0,0 +1,32 @@
+function updateMAp() {
+
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(rsp => {
+            //console.log(rsp)
+            rsp.data.forEach(element => {
+                latitude = element.latitude;
+                longitude = element.longitude;
+
+                cases=element.infected;
+                if (cases>255){
+                    color = "rgb(255,0,0)";
+                }
+                else {
+                    color = `rgb($(cases),$(cases),$(cases))`;
+                }
+
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                    })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map); 
+            });
+
+
+        })
+
+}
+
+updateMAp();
