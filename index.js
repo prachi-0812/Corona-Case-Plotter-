3daf92c23e49770d1c6cceae746e4c3138c1c5eb
Prachi <108670915+prachi-0812@users.noreply.github.com>
2022-07-08 12:38:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function updateMAp() {
-
-    fetch("data.json")
+    console.log("UPdating map")
+    fetch("/data.json")
         .then(response => response.json())
         .then(rsp => {
             //console.log(rsp)
@@ -8,25 +8,60 @@ function updateMAp() {
                 latitude = element.latitude;
                 longitude = element.longitude;
 
-                cases=element.infected;
-                if (cases>255){
-                    color = "rgb(255,0,0)";
+                country = element.name;
+                recovered = element.recovered;
+                death = element.dead;
+                infected = element.infected;
+                vaccinated = element.vaccinated;
+                cases = element.sick;
+
+                if (cases > 100000) {
+                    color = "rgb(102, 0, 0)";
+                }
+                else if (cases > 10000 && cases < 100000) {
+                    color = "rgb(230, 0, 0)";
+                }
+                else if (cases > 5000 && cases < 10000) {
+                    color = "rgb(255, 102, 102)";
+                }
+                else if (cases > 1000 && cases < 5000) {
+                    color = "rgb(255,255,102)";
+                }
+                else if (cases > 100 && cases < 1000) {
+                    color = "rgb(170,255,153)";
                 }
                 else {
-                    color = `rgb($(cases),$(cases),$(cases))`;
+                    color = "rgb(21,128,0)";
                 }
 
-                new mapboxgl.Marker({
-                    draggable: false,
-                    color: color
+
+                
+
+                    new mapboxgl.Marker({
+                        draggable: false,
+                        color: color,
+
                     })
-                    .setLngLat([longitude, latitude])
-                    .addTo(map); 
-            });
+                        .setLngLat([longitude, latitude])
+                        .setPopup(
+                            new mapboxgl.Popup({ offset: 25 }) // add popups
+                                .setHTML(
+
+                                    `<h3>${`Country : ${country}`}</h3>
+                            <p>${`Total Infected : ${infected}`}</p>
+                            <p>${`Present Cases : ${cases}`}</p>
+                            <p>${`Recovered : ${recovered}`}</p>
+                            <p>${`Total deaths : ${death}`}</p>
+                            <p>${`Vaccinated : ${vaccinated}`}</p>`
+                                )
+                        )
+                        .addTo(map);
+                });
 
 
-        })
+            })
 
-}
+        }
 
-updateMAp();
+let interval = 10000;
+setInterval(updateMAp, interval);
